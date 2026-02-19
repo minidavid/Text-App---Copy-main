cca5401f0682779cd758e5f5d2789199639e3395
minidavid <njihiadavid02@gmail.com>
2026-02-19 04:37:51 +0300

    espeak cool to set Volume.

--- searchText.lua
+++ searchText.lua
@@ -1662,7 +1662,8 @@ function Speech(mySpeech)
     -- love.graphics.pop()
 
     local espeakCommand =  'espeak-ng "' .. mySpeech .. '"&'
-    
+    mySpeech:setVolume(volume)
+
     os.execute(espeakCommand)
 
     if cursorIndex > 1 then
